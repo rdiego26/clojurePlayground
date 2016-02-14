c3c15b5c4164726b38049ee9dcb63acc76749b4c
Diego Ramos <rdiego26@gmail.com>
2016-02-14 20:25:53 -0200

    ADD forca

---
+++ forca/src/forca/core.clj
@@ -0,0 +1,40 @@
+(ns forca.core
+  (:gen-class))
+
+(def total-de-vidas 6)
+
+(defn perdeu [] (print "Você perdeu"))
+
+(defn ganhou [] (print "Você ganhou!"))
+
+(defn letras-faltantes [palavras acertos]
+	(remove (fn [letra] (contains? acertos (str letra))) palavras)
+)
+
+(defn acertou-a-palavra-toda? [palavra acertos] 
+	(empty? (letras-faltantes palavra acertos))
+)
+
+(defn jogo [vidas palavra acertos]
+	(if (= vidas 0)
+		(perdeu)
+		(if (acertou-a-palavra-toda? palavra acertos)
+			(ganhou)
+			(print "Chuta, amigo!")
+		)
+		; (do
+		; 	(jogo (- vidas 1))
+		; )
+		
+	)
+)	
+
+(defn fib[x]
+    (if (= x 0) 0 
+    (if (= x 1) 1 
+    (+ (fib (- x 1)) (fib (- x 2))))))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Hello, World!"))
